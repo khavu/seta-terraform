c8abfaf3f2dedae83306c61fca7173190195409f
Vu Kha <vu.kha@imhelo.com>
2019-05-25 04:49:02 +0700

    update

--- global/output.tf
+++ global/output.tf
@@ -1,4 +1,4 @@
 output "Access to Services" {
   description = "Public access to services of instance"
-  value       = ["SSH: ssh -i keys/test_key centos@${aws_instance.test.public_ip} \nPrometheus: http://${aws_instance.test.public_ip}:9090 \nGrafana: http://${aws_instance.test.public_ip}:3000"]
+  value       = ["Please wait 1 minute before accessing the services \n SSH: ssh -i keys/test_key centos@${aws_instance.test.public_ip} \n Prometheus: http://${aws_instance.test.public_ip}:9090 \n Grafana: http://${aws_instance.test.public_ip}:3000"]
 }
